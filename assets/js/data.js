cae822290b125569d29af63656dc7b3967816b81
Jimena Belen <jiment4@hotmail.com>
2023-11-03 20:06:57 -0300

    data.js

---
+++ assets/js/data.js
@@ -0,0 +1,248 @@
+/**array de movimientos*/
+const movimientos = [
+    {
+        "id": "1",
+        "name": "acción",
+        "url": "accion"
+    },
+    {
+        "id": "2",
+        "name": "art brut",
+        "url": "artbrut"
+    },
+    {
+        "id": "3",
+        "name": "art deco",
+        "url": "artdeco"
+    },
+    {
+        "id": "4",
+        "name": "arte abstracto",
+        "url": "arteabstracto"
+    },
+    {
+        "id": "5",
+        "name": "arte cinético",
+        "url": "artecinetico"
+    },
+    {
+        "id": "6",
+        "name": "arte conceptual",
+        "url": "arteconceptual"
+    },
+    {
+        "id": "7",
+        "name": "arte povera",
+        "url": "artepovera"
+    },
+    {
+        "id": "8",
+        "name": "art noveau",
+        "url": "artnoveau"
+    },
+    {
+        "id": "9",
+        "name": "barroco",
+        "url": "barroco"
+    },
+    {
+        "id": "10",
+        "name": "cinquecento",
+        "url": "cinquecento"
+    },
+    {
+        "id": "11",
+        "name": "constructivismo",
+        "url": "constructivismo"
+    },
+    {
+        "id": "12",
+        "name": "cubismo",
+        "url": "cubismo"
+    },
+    {
+        "id": "13",
+        "name": "dadaísmo",
+        "url": "dadaismo"
+    },
+    {
+        "id": "14",
+        "name": "expresionismo",
+        "url": "expresionismo"
+    },
+    {
+        "id": "15",
+        "name": "expresionismo abstracto",
+        "url": "expresionismoabstracto"
+    },
+    {
+        "id": "16",
+        "name": "fauvismo",
+        "url": "fauvismo"
+    },
+    {
+        "id": "17",
+        "name": "futurismo",
+        "url": "futurismo"
+    },
+    {
+        "id": "18",
+        "name": "hiperrealismo",
+        "url": "hiperrealismo"
+    },
+    {
+        "id": "19",
+        "name": "impresionismo",
+        "url": "impresionismo"
+    },
+    {
+        "id": "20",
+        "name": "informalismo",
+        "url": "informalismo"
+    },
+    {
+        "id": "21",
+        "name": "land art",
+        "url": "landart"
+    },
+    {
+        "id": "22",
+        "name": "manierismo",
+        "url": "manierismo"
+    },
+    {
+        "id": "23",
+        "name": "minimalismo",
+        "url": "minimalismo"
+    },
+    {
+        "id": "24",
+        "name": "neoclasicismo",
+        "url": "neoclasicismo"
+    },
+    {
+        "id": "25",
+        "name": "neoplasticismo",
+        "url": "neoplasticismo"
+    },
+    {
+        "id": "26",
+        "name": "nouveau réalisme",
+        "url": "nouveaurealisme"
+    },
+    {
+        "id": "27",
+        "name": "nueva figuración",
+        "url": "nuevafiguracion"
+    },
+    {
+        "id": "28",
+        "name": "nueva objetividad",
+        "url": "nuevaobjetividad"
+    },
+    {
+        "id": "29",
+        "name": "nuevo realismo",
+        "url": "nuevorealismo"
+    },
+    {
+        "id": "30",
+        "name": "op art",
+        "url": "opart"
+    },
+    {
+        "id": "31",
+        "name": "orfismo",
+        "url": "orfismo"
+    },
+    {
+        "id": "32",
+        "name": "orientalismo",
+        "url": "orientalismo"
+    },
+    {
+        "id": "33",
+        "name": "pintura metafísica",
+        "url": "pinturametafisica"
+    },
+    {
+        "id": "34",
+        "name": "pop art",
+        "url": "popart"
+    },
+    {
+        "id": "35",
+        "name": "post impresionismo",
+        "url": "postimpresionismo"
+    },
+    {
+        "id": "36",
+        "name": "post modernidad",
+        "url": "postmodernidad"
+    },
+    {
+        "id": "37",
+        "name": "precisionismo",
+        "url": "precisionismo"
+    },
+    {
+        "id": "38",
+        "name": "primitivismo",
+        "url": "primitivismo"
+    },
+    {
+        "id": "39",
+        "name": "proto-renacimiento",
+        "url": "protorenacimiento"
+    },
+    {
+        "id": "40",
+        "name": "quattrocento",
+        "url": "quattrocento"
+    },
+    {
+        "id": "41",
+        "name": "realismo",
+        "url": "realismo"
+    },
+    {
+        "id": "42",
+        "name": "realismo social",
+        "url": "realismosocial"
+    },
+    {
+        "id": "43",
+        "name": "renacimiento del norte de europa",
+        "url": "rena-nor-euro"
+    },
+    {
+        "id": "44",
+        "name": "rococó",
+        "url": "rococo"
+    },
+    {
+        "id": "45",
+        "name": "romanticismo",
+        "url": "romanticismo"
+    },
+    {
+        "id": "46",
+        "name": "simbolismo",
+        "url": "simbolismo"
+    },
+    {
+        "id": "47",
+        "name": "street art",
+        "url": "streetart"
+    },
+    {
+        "id": "48",
+        "name": "suprematismo",
+        "url": "suprematismo"
+    },
+    {
+        "id": "49",
+        "name": "surrealismo",
+        "url": "surrealismo"
+    }
+];
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    mostrarMov(movimientos);
     buscarMov()
     showTarjeta();
     closeTarjeta();
@@ -32,21 +33,14 @@ document.addEventListener("DOMContentLoaded", () => {
     btnClose.addEventListener("click", closeTarjeta);
 
 
-    /**array de movimientos*/
-    const movimientos = ["accion", "art brut", "art deco", "arte abstracto", "arte cinetico", "arte conceptual", "arte povera", "art noveau", "barroco",
-    "cinquecento", "constructivismo", "cubismo", "dadaismo", "expresionismo", "expresionismo abstracto", "fauvismo", "futurismo", "hiperrealismo"
-    , "impresionismo", "informalismo", "land art", "manierismo", "minimalismo", "neoclasicismo", "neoplasticismo", "nouveau realisme", "nueva figuracion",
-    "nueva objetividad", "nuevo realismo", "op art", "orfismo", "orientalismo", "pintura metafisica", "pop art", "post impresionismo", 
-    "post impresionismo", "post modernidad", "precisionismo", "primitivismo", "proto-renacimiento", "quattrocento", "realismo",
-    "realismo social", "renacimiento del norte de europa", "rococo", "romaticismo", "simbolismo", "street art", "suprematismo", "surrealismo"];
-   
+
     /**funcion para mostrar movimientos**/
 
     function mostrarMov(movimientos){
 
         movimientos.forEach(movimiento => {
             const elements = document.createElement("li");
-            elements.innerHTML =  `<a href="#"><strong>${movimiento}</strong></a>`;
+            elements.innerHTML =  `<a href="../movimientos/${movimiento.url}.html"><strong>${movimiento.name}</strong></a>`;
             boxContent.appendChild(elements);      
         })
     }
@@ -58,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const inputText = e.target.value.toLowerCase().trim() /* cada vez que se ingresen valores en el input se guardaran en esta variable, en minuscula y quitando espacios en blanco**/ 
 
             const mostrarFiltrado = movimientos.filter( movimiento => 
-                movimiento.toLowerCase().startsWith(inputText)
+                movimiento.name.toLowerCase().startsWith(inputText)
             );
             
             if (mostrarFiltrado.length) {
